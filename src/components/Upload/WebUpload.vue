2112b40fb49c112b2db54dacfe52408ee8761b8a
Yzerost <924778110@qq.com>
2021-07-27 18:41:04 +0800

    更改上验重功能

--- src/components/Upload/WebUpload.vue
+++ src/components/Upload/WebUpload.vue
@@ -253,10 +253,10 @@ export default {
               success: function(response) {
                 if (response) {
                   that.uploader.skipFile(file)
-                  that.setTableBtn(file.name, '文件已存在')
+                  // that.setTableBtn(file.name, '文件已存在')
                   that.$notify.success({
-                    showClose: true,
-                    message: `[ ${file.name} ]文件秒传`
+                    // showClose: true,
+                    message: `[ ${file.name} ]文件已存在`
                   })
                   // 如果存在，则跳过该文件，秒传成功
                   that.fList.push(response.data)
